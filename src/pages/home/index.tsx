d71b7ab69cfd4e31d12558060557945ff140e9e5
João Paulo Fricks <jpfricks@gmail.com>
2023-02-02 10:10:25 -0300

    chore: Create home layout 🍩

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import { Heading, Text } from '@ignite-ui/react'
+import { Container, Hero, Preview } from './styles'
+
+import previewImage from '../../assets/app-preview.png'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading size="4xl">Agendamento descomplicado</Heading>
+        <Text size="xl">
+          Conecte seu calendário e permita que as pessoas marquem agendamentos
+          no seu tempo livre.
+        </Text>
+      </Hero>
+      <Preview>
+        <Image
+          src={previewImage}
+          height={402}
+          quality={100}
+          priority
+          alt="Preview do calendário"
+        />
+      </Preview>
+    </Container>
+  )
+}
