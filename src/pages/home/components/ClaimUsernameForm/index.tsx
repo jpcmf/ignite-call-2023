cac37323efcae06e4a58221b8bdeb1663af12ca2
João Paulo Fricks <jpfricks@gmail.com>
2023-02-08 22:29:09 -0300

    chore: Add react-hook-form and zod in ClaimUsernameForm component 🍩

--- src/pages/home/components/ClaimUsernameForm/index.tsx
+++ src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,12 +1,31 @@
 import { Button, TextInput } from '@ignite-ui/react'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
 import { Form } from './styles'
 
+const handleWithUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof handleWithUsernameFormSchema>
+
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handleClaimUsername(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
     <div>
-      <Form as="form">
-        <TextInput size="sm" prefix="ignite.com/" placeholder="seu-usuario" />
+      <Form as="form" onSubmit={handleSubmit(handleClaimUsername)}>
+        <TextInput
+          size="sm"
+          prefix="ignite.com/"
+          placeholder="seu-usuario"
+          {...register('username')}
+        />
         <Button size="sm" type="submit">
           Reservar
           <ArrowRight />
