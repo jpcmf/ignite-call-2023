861b28eeef110de9533dde00c9e81fcd8af32800
João Paulo Fricks <jpfricks@gmail.com>
2023-03-03 11:42:04 -0300

    chore: Handle with error using useSession 🌚

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -1,15 +1,20 @@
-import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/router'
-import { ArrowRight } from 'phosphor-react'
+import { ArrowRight, Check } from 'phosphor-react'
 import { Container, Header } from '../styles'
+import { signIn, useSession } from 'next-auth/react'
 import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { AuthError, ConnectBox, ConnectItem } from './styles'
 
 export default function Register() {
+  const session = useSession()
   const router = useRouter()
 
   const hasAuthError = !!router.query.error
-  //   async function handleRegister() {}
+  const isSignedIn = session.status === 'authenticated'
+
+  async function handleConnectCalendar() {
+    await signIn('google', { callbackUrl: '/register/connect-calendar' })
+  }
 
   return (
     <Container>
@@ -26,13 +31,19 @@ export default function Register() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button
-            variant="secondary"
-            size="sm"
-            onClick={() => signIn('google')}
-          >
-            Conectar <ArrowRight />
-          </Button>
+          {isSignedIn ? (
+            <Button size="sm" disabled>
+              Conectado <Check />
+            </Button>
+          ) : (
+            <Button
+              variant="secondary"
+              size="sm"
+              onClick={handleConnectCalendar}
+            >
+              Conectar <ArrowRight />
+            </Button>
+          )}
         </ConnectItem>
 
         {hasAuthError && (
@@ -41,7 +52,7 @@ export default function Register() {
             permissões de acesso ao Google Calendar.
           </AuthError>
         )}
-        <Button type="submit">
+        <Button type="submit" disabled={!isSignedIn}>
           Próximo passo <ArrowRight />
         </Button>
       </ConnectBox>
