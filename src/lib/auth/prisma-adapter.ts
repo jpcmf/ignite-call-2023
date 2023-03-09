f85ebc31dee5e00058ebecdf6cbff83f3b421721
João Paulo Fricks <jpfricks@gmail.com>
2023-03-08 22:38:26 -0300

    chore: Handle with cookies and createUser() in prisma adapter 🌚

--- src/lib/auth/prisma-adapter.ts
+++ src/lib/auth/prisma-adapter.ts
@@ -1,9 +1,44 @@
+import { NextApiRequest, NextApiResponse } from 'next'
 import { Adapter } from 'next-auth/adapters'
+import { destroyCookie, parseCookies } from 'nookies'
 import { prisma } from '../prisma'
 
-export default function PrismaAdapter(): Adapter {
+export default function PrismaAdapter(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Adapter {
   return {
-    async createUser(user) {},
+    async createUser(user) {
+      const { '@ignitecall:userId': userIdOnCookies } = parseCookies({ req })
+
+      if (!userIdOnCookies) {
+        throw new Error('User ID not found on cookies.')
+      }
+
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: userIdOnCookies,
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url: user.avatar_url,
+        },
+      })
+
+      destroyCookie({ res }, '@ignitecall:userId', {
+        path: '/',
+      })
+
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        username: prismaUser.username,
+        email: prismaUser.email!,
+        avatar_url: prismaUser.avatar_url!,
+        emailVerified: null,
+      }
+    },
 
     async getUser(id) {
       const user = await prisma.user.findUniqueOrThrow({ where: { id } })
@@ -143,7 +178,13 @@ export default function PrismaAdapter(): Adapter {
         expires: prismaSession.expires,
       }
     },
-    // async deleteSession(sessionToken) {},
+    async deleteSession(sessionToken) {
+      await prisma.session.delete({
+        where: {
+          session_token: sessionToken,
+        },
+      })
+    },
     // async createVerificationToken({ identifier, expires, token }) {},
     // async useVerificationToken({ identifier, token }) {},
   }
