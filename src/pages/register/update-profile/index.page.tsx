7606155fc4e3b599efdb1ac61fd0e1ba171b0736
João Paulo Fricks <jpfricks@gmail.com>
2023-03-22 09:15:08 -0300

    chore: Create update profile layout 🍩

---
+++ src/pages/register/update-profile/index.page.tsx
@@ -0,0 +1,54 @@
+import { z } from 'zod'
+// import { api } from '@/src/lib/axios'
+import { useForm } from 'react-hook-form'
+import { ArrowRight } from 'phosphor-react'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Container, Header } from '../styles'
+import { Button, Heading, MultiStep, Text, TextArea } from '@ignite-ui/react'
+import { FormAnnotation, ProfileBox } from './styles'
+
+const handleWithUpdateProfileFormSchema = z.object({
+  bio: z.string(),
+})
+
+type UpdateProfileFormData = z.infer<typeof handleWithUpdateProfileFormSchema>
+
+export default function UpdateProfile() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<UpdateProfileFormData>({
+    resolver: zodResolver(handleWithUpdateProfileFormSchema),
+  })
+
+  async function handleUpdateProfile(data: UpdateProfileFormData) {}
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Bem-vindo ao Ignite Call.</Heading>
+        <Text>Por último, uma breve descrição e uma foto de perfil.</Text>
+
+        <MultiStep size={4} currentStep={1} />
+      </Header>
+
+      <ProfileBox as="form" onSubmit={handleSubmit(handleUpdateProfile)}>
+        <label>
+          <Text size="sm">Foto de perfil</Text>
+        </label>
+        <label>
+          <Text size="sm">Sobre você</Text>
+          <TextArea {...register('bio')} />
+          <FormAnnotation size="sm">
+            Fale um pouco sobre você. Isto será exibido em sua página pessoal.
+          </FormAnnotation>
+        </label>
+
+        <Button type="submit" disabled={isSubmitting}>
+          Finalizar <ArrowRight />
+        </Button>
+      </ProfileBox>
+    </Container>
+  )
+}
