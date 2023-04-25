ee6c177f1b12452880a6ba5787d783d1aed719df
João Paulo Fricks <jpfricks@gmail.com>
2023-04-25 10:23:06 -0300

    chore: Create schedule form layout and calendar component 🌚

---
+++ src/components/Calendar/styles.ts
@@ -0,0 +1,19 @@
+import { Text, styled } from '@ignite-ui/react'
+
+export const CalendarContainer = styled('div', {
+  margin: '$6 auto 0',
+  padding: 0,
+  display: 'grid',
+  maxWidth: '100%',
+  position: 'relative',
+})
+
+export const CalendarHeader = styled('div', {})
+
+export const CalendarTitle = styled(Text, {})
+
+export const CalendarActions = styled('div', {})
+
+export const CalendarBody = styled('table', {})
+
+export const CalendarDay = styled('button', {})
