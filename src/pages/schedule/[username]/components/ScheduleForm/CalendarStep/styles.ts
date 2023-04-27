51eb5f51321988daee14a6f5989427e0b1e7e7e1
João Paulo Fricks <jpfricks@gmail.com>
2023-04-27 08:42:13 -0300

    chore: Add structure of time picker 🍩

--- src/pages/schedule/[username]/components/ScheduleForm/CalendarStep/styles.ts
+++ src/pages/schedule/[username]/components/ScheduleForm/CalendarStep/styles.ts
@@ -1,4 +1,4 @@
-import { Box, styled } from '@ignite-ui/react'
+import { Box, Text, styled } from '@ignite-ui/react'
 
 export const Container = styled(Box, {
   margin: '$6 auto 0',
@@ -7,6 +7,27 @@ export const Container = styled(Box, {
   maxWidth: '100%',
   position: 'relative',
 
-  width: 540,
-  gridTemplateColumns: '1fr',
+  variants: {
+    isTimePickerOpen: {
+      true: {
+        gridTemplateColumns: '1fr 280px',
+
+        '@media(max-width: 900px)': {
+          gridTemplateColumns: '1fr',
+        },
+      },
+      false: {
+        width: 540,
+        gridTemplateColumns: '1fr',
+      },
+    },
+  },
 })
+
+export const TimePicker = styled('div', {})
+
+export const TimePickerHeader = styled(Text, {})
+
+export const TimePickerList = styled('div', {})
+
+export const TimePickerItem = styled('button', {})
