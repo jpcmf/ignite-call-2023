711bf8f32e59f219d84ed8a5c024e1e50fb68b2c
João Paulo Fricks <jpfricks@gmail.com>
2023-05-04 08:53:06 -0300

    chore: Add the related date in calendar to schedule table 🌚

--- src/pages/schedule/[username]/components/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/components/ScheduleForm/CalendarStep/index.tsx
@@ -7,11 +7,17 @@ import {
   TimePickerList,
 } from './styles'
 import { useState } from 'react'
+import dayjs from 'dayjs'
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
 
   const isDateSelected = !!selectedDate
+
+  const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
+  const weekDate = selectedDate
+    ? dayjs(selectedDate).format('DD[ de ]MMMM')
+    : null
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
@@ -20,7 +26,7 @@ export function CalendarStep() {
       {isDateSelected && (
         <TimePicker>
           <TimePickerHeader>
-            quinta-feira, <span>27 de abril</span>
+            {weekDay}, <span>{weekDate}</span>
           </TimePickerHeader>
           <TimePickerList>
             <TimePickerItem>08:00h</TimePickerItem>
