4c695107e3cc7b4c0949bdf13503c4a74fa66f4e
João Paulo Fricks <jpfricks@gmail.com>
2023-05-06 19:21:07 -0300

    chore: Load availabe hours when select a date in calendar 🌚

--- src/pages/schedule/[username]/components/ScheduleForm/CalendarStep/index.tsx
+++ src/pages/schedule/[username]/components/ScheduleForm/CalendarStep/index.tsx
@@ -1,4 +1,7 @@
-import { Calendar } from '@/src/components/Calendar'
+import dayjs from 'dayjs'
+import { api } from '@/src/lib/axios'
+import { useRouter } from 'next/router'
+import { useEffect, useState } from 'react'
 import {
   Container,
   TimePicker,
@@ -6,18 +9,32 @@ import {
   TimePickerItem,
   TimePickerList,
 } from './styles'
-import { useState } from 'react'
-import dayjs from 'dayjs'
+import { Calendar } from '@/src/components/Calendar'
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  const [availability, setAvailability] = useState(null)
+  const router = useRouter()
 
   const isDateSelected = !!selectedDate
+  const username = router.query.username
 
   const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
   const weekDate = selectedDate
     ? dayjs(selectedDate).format('DD[ de ]MMMM')
     : null
+
+  useEffect(() => {
+    if (!selectedDate) return
+
+    api
+      .get(`/users/${username}/availability`, {
+        params: { date: dayjs(selectedDate).format('YYYY-MM-DD') },
+      })
+      .then((response) => {
+        console.log(response.data)
+      })
+  }, [selectedDate, username])
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
