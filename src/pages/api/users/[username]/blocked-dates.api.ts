5001abd3a71617cb9787ecf0859a9e56425cf1ae
João Paulo <jpfricks@gmail.com>
2023-05-11 23:11:28 -0300

    chore: Fix blockedDatesRaw query 🔥

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -48,7 +48,9 @@ export default async function handle(
   const blockedDatesRaw = await prisma.$queryRaw`
     SELECT * FROM schedulings S
 
-    WHERE S.user_id = ${user?.id} AND DATE_FORMAT(S.date, '%Y-%m') = ${year}-${month}
+    WHERE S.user_id = ${
+      user?.id
+    } AND DATE_FORMAT(S.date, '%Y-%m') = ${`${year}-${month}`}
   `
 
   return res.json({ blockedWeekDays, blockedDatesRaw })
