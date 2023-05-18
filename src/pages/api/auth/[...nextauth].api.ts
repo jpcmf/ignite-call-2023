b59ff9146a95d633d609c2bbd1e25b08c034333d
João Paulo <jpfricks@gmail.com>
2023-05-17 23:25:14 -0300

    chore: Fix code 🌚

--- src/pages/api/auth/[...nextauth].api.ts
+++ src/pages/api/auth/[...nextauth].api.ts
@@ -17,7 +17,7 @@ export function buildNextAuthOptions(
           params: {
             prompt: 'consent',
             access_type: 'offline',
-            response_type:'code',
+            response_type: 'code',
             scope:
               'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
           },
